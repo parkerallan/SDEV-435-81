6d0be023e0ca1d105e22855720809d3818ef30fc
parkerallan <parkerallan2000@gmail.com>
2024-09-25 12:39:40 -0500

    alternate view

--- pages/views.py
+++ pages/views.py
@@ -8,11 +8,27 @@ from django.contrib.auth.decorators import login_required
 def home_view(request):
   return render(request, 'home.html')
 
+def login_view(request):
+  if request.method == 'POST':
+      username = request.POST.get('username')
+      password = request.POST.get('password')
+      user = authenticate(username=username, password=password)
+      if user:
+          login(request, user)
+          return redirect('feed')
+  if request.user.is_authenticated:
+      return redirect('feed')
+  else:
+      return render(request, 'login.html')
+
 def about_view(request):
   return render(request, 'about.html')
 
 def registered_view(request):
   return render(request,'registered.html')
+
+def redirect_to_website(request):
+  return redirect('https://www.parkerdev.net/#contact')
 
 @login_required(login_url='/login/')
 def feed_view(request):

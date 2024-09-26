de9df574225e3fa543b0f743476554b2da61b68f
parkerallan <parkerallan2000@gmail.com>
2024-09-26 17:36:40 -0500

    update imports and change view

--- social_media_app/urls.py
+++ social_media_app/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from pages.views import *
+from posts.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
@@ -11,8 +12,7 @@ urlpatterns = [
     path('about/', about_view, name='about'),
     path('contact/', redirect_to_website, name='contact'),
     
-    path('feed/', feed_view, name='feed'),
-    # path('post/', name='post'),
+    path('feed/', post_list, name='feed'),
     path('posts/', include('posts.urls')),
     
     path('register/', register_view, name='register'),

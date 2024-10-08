6f1431a74111d9777a1ecce243c13b0728d452dd
parkerallan <parkerallan2000@gmail.com>
2024-10-08 02:08:11 -0500

    update form with profile attributes

--- social_media_app/forms.py
+++ social_media_app/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from pages.models import Profile
 
 class RegistrationForm(UserCreationForm):
   email = forms.EmailField(required=True)
@@ -8,3 +9,13 @@ class RegistrationForm(UserCreationForm):
   class Meta:
     model = User
     fields = ['username', 'email', 'password1', 'password2']
+    
+class UserUpdateForm(forms.ModelForm):
+  class Meta:
+      model = User
+      fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+  class Meta:
+      model = Profile
+      fields = ['display_name', 'bio', 'social_links']

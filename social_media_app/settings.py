9b9960bcb5d06a65c9358a1b2059413afd010754
parkerallan <parkerallan2000@gmail.com>
2024-11-01 01:14:51 -0500

    another typo

--- social_media_app/settings.py
+++ social_media_app/settings.py
@@ -17,7 +17,7 @@ import dj_database_url
 from environ import Env
 env = Env()
 Env.read_env()
-ENVIRONMENT = env('ENVIROMENT', default='production')
+ENVIRONMENT = env('ENVIRONMENT', default='production')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent

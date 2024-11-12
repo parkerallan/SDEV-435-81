60783ea1b51c3a36f98ab27095004b7b2cff2550
parkerallan <parkerallan2000@gmail.com>
2024-11-12 04:31:24 -0600

    helper comments

--- pages/signals.py
+++ pages/signals.py
@@ -1,3 +1,6 @@
+# Description: This file is used to create or save a profile for each user that is created.
+# This is done by creating a signal that listens for when a user is created or saved.
+
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
@@ -6,8 +9,8 @@ from .models import Profile
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user=instance)
+        Profile.objects.create(user=instance)           # Create a profile for the user
 
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
-    instance.profile.save()
+    instance.profile.save()                             # Save the profile for the user
